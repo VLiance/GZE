64e1bb53594bdbd5f8f3d01df0245ae0894de381
maeiky <maeiky@gmail.com>
2020-03-20 00:16:00 -0400

    Fix Copy func

--- src/Lib_GZ/3rdparty/Font/stb_truetype.hxx
+++ src/Lib_GZ/3rdparty/Font/stb_truetype.hxx
@@ -15,6 +15,27 @@
 	#define stb_img_free free
 #endif
 */
+
+
+/*
+   // #define your own functions "STBTT_malloc" / "STBTT_free" to avoid malloc.h
+   #ifndef STBTT_malloc
+   #include <stdlib.h>
+   #define STBTT_malloc(x,u)  ((void)(u),malloc(x))
+   #define STBTT_free(x,u)    ((void)(u),free(x))
+   #endif
+*/
+  #include <stdlib.h>
+  
+ /*
+ inline void* myMalloc(gzUInt _nSize){
+	 printf("\nMalloc: %d", _nSize );
+	 return malloc(_nSize*2);
+ }
+#define STBTT_malloc(x,u)  ((void)(u),myMalloc(x))
+#define STBTT_free(x,u)    ((void)(u),free(x))
+
+*/
 #include "stb_truetype.h"
 
 
